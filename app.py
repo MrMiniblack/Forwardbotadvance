ba532201fa273f837caaf2eba987193008a781c9
adityabotdeployer <120628250+adityabotdeployer@users.noreply.github.com>
2023-02-04 11:42:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'GreyMatters_Bot'
+
+#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
+os.system("git clone https://Nickroger797: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@github.com/Nickroger797/filetolinkbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
