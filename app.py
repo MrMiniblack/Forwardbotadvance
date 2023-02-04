cc74fe6344b206eb0e72af872cf3b25e280ce9e4
adityabotdeployer <120628250+adityabotdeployer@users.noreply.github.com>
2023-02-04 11:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://Nickroger797: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@github.com/Nickroger797/filetolinkbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
+os.system("git clone https://github.com/adityabotdeployer/Forwardbotadvance okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
